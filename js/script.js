83dc7a6883973441e9a8f2bb0c6b03f73f8fd25a
maicol <maicol.matteoli@gmail.com>
2021-06-22 16:55:20 +0200

    add : vue test img

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    
+    el: '#app',
+    
+    data:{
+       immagini = ['../img/img1.jpg','../img/img2.jpg','../img/img3.jpg','../img/img4.jpg']
+    },
+
+    metods:{
+
+    }
+
+});
